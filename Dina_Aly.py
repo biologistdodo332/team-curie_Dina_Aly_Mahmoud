e1c57ba943b95567d61b6817d185ad086bfacb05
biologistdodo332 <69098237+biologistdodo332@users.noreply.github.com>
2020-08-02 18:51:39 +0200

    Add files via upload

---
+++ Dina_Aly.py
@@ -0,0 +1,11 @@
+name = input ("what is your name ?" )
+N =  str (name )
+print ("my name is " ,  N )
+E_mail = input ("enter you main E-mail" )
+E = str (E_mail)
+print ("my E-mail is ", E )
+W = input (" enter your slack username" )
+S = str (W)
+print ("my  slack username is " , S)
+
+ 
